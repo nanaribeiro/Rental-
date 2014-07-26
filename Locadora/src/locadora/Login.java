9887710022a4a23fd8f582a79cdde48a07d7c1c1
nanaribeiro <alanaribeiroramos@gmail.com>
2014-07-25 23:10:09 -0300

    Login alterado de JInternalFrame para JDialog. Quando o usuário digitar o login correto o sistema irá inicializar. Não consegui bloquear o JFrame que estava adicionando a tela de login, para o sistema só funcionar depois do login. Como é uma questão estética do jeito que consegui foi bom.

--- Locadora/src/locadora/Login.java
+++ Locadora/src/locadora/Login.java
@@ -7,40 +7,88 @@
 package locadora;
 
 
-import java.awt.BorderLayout;
-import static java.awt.Frame.MAXIMIZED_BOTH;
+import BancoDados.InteracaoDAO;
+
+import java.awt.*;
 import java.awt.event.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
+
 
 /**
  *
  * @author alana.sasse
  */
 public class Login implements ActionListener{
-    JFrame frmPrincipal=new JFrame();
-    JInternalFrame frmLogin=new JInternalFrame();
-    JDesktopPane desktop=new JDesktopPane();
-    JButton login = new JButton("Login");
-    JLabel label = new JLabel(frmLogin.getTitle(), JLabel.CENTER);
-    void login() {
-        login.addActionListener((ActionListener) this);
-        frmLogin.setBounds(25,25,300,300);
-        frmPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frmLogin.add(label, BorderLayout.CENTER);
-        frmLogin.getContentPane().add(login);
-        desktop.add(frmLogin);
+    JDialog frmLogin=new JDialog();
+    String sql;
+    String strAux;
+    boolean test;
+    JButton cmdLogin = new JButton("Login");
+    private JLabel lblMatricula, lblSenha;
+    JTextField txtMatricula= new JTextField(15);
+    private JPasswordField pswSenha;
+    void LayoutLogin() {
+        
+        frmLogin.getContentPane().setLayout(new FlowLayout());
+        cmdLogin.addActionListener(this);
+        frmLogin.setBounds(50,50,300,300);
+        lblMatricula=new JLabel("Matricula");
+        lblSenha=new JLabel("Senha      ");
+        pswSenha=new JPasswordField(6);
+        frmLogin.getContentPane().add(lblMatricula);
+        frmLogin.getContentPane().add(txtMatricula);
+        frmLogin.getContentPane().add(lblSenha);
+        frmLogin.getContentPane().add(pswSenha);
+        frmLogin.getContentPane().add(cmdLogin);
+        frmLogin.setUndecorated(true);
         frmLogin.setVisible(true);
-        frmPrincipal.add(desktop, BorderLayout.CENTER);
-        frmPrincipal.setExtendedState(MAXIMIZED_BOTH);
-        frmPrincipal.setVisible(true);
         
       
     }
+    @Override
     public void actionPerformed(ActionEvent event){
         /*Quando o funcionário clicar em login os dados serão comparados
         com os dados do banco de dados, se forem iguais o usuário acessará
         a tela principal
         */
         
+        if (event.getSource() == cmdLogin){
+          
+            //Se o usuário não digitar a matricula não será executado as ações do botão
+            if (txtMatricula.getText() != null && !txtMatricula.getText().equals("")) {
+                try {
+                    InteracaoDAO teste= new InteracaoDAO();
+                    ResultSet RS;
+                    sql="Select * from funcionarios where Matricula= " + Integer.parseInt(txtMatricula.getText())+";";
+                    RS = teste.InteracaoDAO(sql);
+                    while(RS.next()){
+                        System.out.println(RS.getInt("Matricula"));
+                        if(Integer.parseInt(txtMatricula.getText())==(RS.getInt("Matricula"))){
+                            System.out.println(RS.getInt("Matricula"));
+                            frmLogin.dispose();
+                            test=true;
+                            Principal unlock=new Principal();
+                            //unlock.frmPrincipal.setEnabled(true);
+                            unlock.layout(true);
+                            
+                    }
+                        else{
+                            JOptionPane.showMessageDialog(null, "Login inválido!");
+                        }
+                    }
+                    teste.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+                }
+               
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Digite a matrícula");
+            }
+        }
     }
 }
